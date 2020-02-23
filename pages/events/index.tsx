32dcf6e49ecde2dd7f888a925aebb2d5922f4284
M Nef <howtoeatpineapples@gmail.com>
2020-02-23 08:54:25 +0800

    refactor(events page): replaced hero banner on the events page with new Hero component

--- pages/events/index.tsx
+++ pages/events/index.tsx
@@ -6,15 +6,12 @@ import Events from '../../components/Events'
 import workshopEvents from '../../data/events_workshop.json'
 import industryEvents from '../../data/events_industry.json'
 import socialEvents from '../../data/events_social.json'
+import Hero from '../../components/Hero'
 
 export default () => (
   <>
     {style}
-    <Jumbotron className='bg-dark text-white d-flex align-items-center rounded-0'>
-      <Container style={{ width: '90%' }}>
-        <h1>Events</h1>
-      </Container>
-    </Jumbotron>
+    <Hero title='Events' />
     <Container>
       <div className='d-flex justify-content-center m-4'>
         <a href='#Workshops' className='link-unstyled'><Button className='d-none d-md-block btn btn-outline-primary link-unstyled quick-link'>Workshops</Button></a>
@@ -34,11 +31,6 @@ export default () => (
 
 const style = (
   <style jsx>{`
-    .jumbotron {
-      margin-top: 64px;
-      height: 300px;
-    }
-
     .quick-link {
       margin: 0 5px;
     }
